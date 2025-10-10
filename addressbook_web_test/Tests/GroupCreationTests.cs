de66f6b20c87ea6ab101d87561a8388d90a2d453
Ruslan <apostaterussle@gmail.com>
2025-10-10 19:55:41 +0300

    Error_with_array

--- addressbook_web_test/Tests/GroupCreationTests.cs
+++ addressbook_web_test/Tests/GroupCreationTests.cs
@@ -10,9 +10,9 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        public static IEnumerable<GroupData> RandomGroupDataProvider ()
+        public static IEnumerable<GroupData> RandomGroupDataProvider()
         {
-            List<GroupData> groups= new List<GroupData>();
+            List<GroupData> groups = new List<GroupData>();
             for (int i = 0; i < 5; i++)
             {
                 groups.Add(new GroupData(GenerateRandomString(15))
@@ -24,23 +24,23 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        //public static IEnumerable<GroupData> GroupDataFromFile()
-        //{
-        //    List<GroupData> groups = new List<GroupData>();
-        //    string[] lines = File.ReadAllLines(@"groups.csv");
-        //    foreach (string l in lines)
-        //    {
-        //        string[] parts = l.Split(',');
-        //        groups.Add(new GroupData(parts[0])
-        //            {
-        //                Header = parts[1],
-        //                Footer = parts[2]
-        //            });
-        //    }
-        //    return groups;
-        //}
-        
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            }
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreateTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
